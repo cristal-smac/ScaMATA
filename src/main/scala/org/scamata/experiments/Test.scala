be02b739c59aac5238de42de5e756022690c8c4c
morge <Maxime.Morge@univ-lille1.fr>
2018-05-07 20:54:05 +0200

    allow boxplot with median rather than mean

--- src/main/scala/org/scamata/experiments/Test.scala
+++ src/main/scala/org/scamata/experiments/Test.scala
@@ -97,16 +97,16 @@ object Test {
           swapSolverTime = swapSolverTime.sortWith(_ < _)
           distributedGiftSolverTime = distributedGiftSolverTime.sortWith(_ < _)
           bw.write(
-            s"$m,$n,${giftSolverRule.min},${giftSolverRule(nbPb/4)},${giftSolverRule.sum/nbPb},${giftSolverRule(nbPb*3/4)},${giftSolverRule.max}" +
-            s"${distributedGiftSolverRule.min},${distributedGiftSolverRule(nbPb/4)},${distributedGiftSolverRule.sum/nbPb},${distributedGiftSolverRule(nbPb*3/4)},${distributedGiftSolverRule.max}" +
-            s"${swapSolverRule.min},${swapSolverRule(nbPb/4)},${swapSolverRule.sum/nbPb},${swapSolverRule(nbPb*3/4)},${swapSolverRule.max}" +
-            s"${lpSolverRule.min},${lpSolverRule(nbPb/4)},${lpSolverRule.sum/nbPb},${lpSolverRule(nbPb*3/4)},${lpSolverRule.max}" +
-            s"${giftSolverTime.min},${giftSolverTime(nbPb/4)},${giftSolverTime.sum/nbPb},${giftSolverTime(nbPb*3/4)},${giftSolverTime.max}" +
-            s"${distributedGiftSolverTime.min},${distributedGiftSolverTime(nbPb/4)},${distributedGiftSolverTime.sum/nbPb},${distributedGiftSolverTime(nbPb*3/4)},${distributedGiftSolverTime.max}" +
-            s"${swapSolverTime.min},${swapSolverTime(nbPb/4)},${swapSolverTime.sum/nbPb},${swapSolverTime(nbPb*3/4)},${swapSolverTime.max}" +
-            s"${lpSolverTime.min},${lpSolverTime(nbPb/4)},${lpSolverTime.sum/nbPb},${lpSolverTime(nbPb*3/4)},${lpSolverTime.max}" +
-            s"${lpSolverPreTime.min},${lpSolverPreTime(nbPb/4)},${lpSolverPreTime.sum/nbPb},${lpSolverPreTime(nbPb*3/4)},${lpSolverPreTime.max}" +
-            s"${lpSolverPostTime.min},${lpSolverPostTime(nbPb/4)},${lpSolverPostTime.sum/nbPb},${lpSolverPostTime(nbPb/4)},${lpSolverPostTime.max}" +
+            s"$m,$n,${giftSolverRule.min},${giftSolverRule(nbPb/4)},${giftSolverRule(nbPb/2)},${giftSolverRule(nbPb*3/4)},${giftSolverRule.max}" +
+            s"${distributedGiftSolverRule.min},${distributedGiftSolverRule(nbPb/4)},${distributedGiftSolverRule(nbPb/2)},${distributedGiftSolverRule(nbPb*3/4)},${distributedGiftSolverRule.max}" +
+            s"${swapSolverRule.min},${swapSolverRule(nbPb/4)},${swapSolverRule(nbPb/2)},${swapSolverRule(nbPb*3/4)},${swapSolverRule.max}" +
+            s"${lpSolverRule.min},${lpSolverRule(nbPb/4)},${lpSolverRule(nbPb/2)},${lpSolverRule(nbPb*3/4)},${lpSolverRule.max}" +
+            s"${giftSolverTime.min},${giftSolverTime(nbPb/4)},${giftSolverTime(nbPb/2)},${giftSolverTime(nbPb*3/4)},${giftSolverTime.max}" +
+            s"${distributedGiftSolverTime.min},${distributedGiftSolverTime(nbPb/4)},${distributedGiftSolverTime(nbPb/2)},${distributedGiftSolverTime(nbPb*3/4)},${distributedGiftSolverTime.max}" +
+            s"${swapSolverTime.min},${swapSolverTime(nbPb/4)},${swapSolverTime(nbPb/2)},${swapSolverTime(nbPb*3/4)},${swapSolverTime.max}" +
+            s"${lpSolverTime.min},${lpSolverTime(nbPb/4)},${lpSolverTime(nbPb/2)},${lpSolverTime(nbPb*3/4)},${lpSolverTime.max}" +
+            s"${lpSolverPreTime.min},${lpSolverPreTime(nbPb/4)},${lpSolverPreTime(nbPb/2)},${lpSolverPreTime(nbPb*3/4)},${lpSolverPreTime.max}" +
+            s"${lpSolverPostTime.min},${lpSolverPostTime(nbPb/4)},${lpSolverPostTime(nbPb/2)},${lpSolverPostTime(nbPb/4)},${lpSolverPostTime.max}" +
             s"${deal/nbPb}," +
             s"${nbPropose/nbPb}, ${nbAccept/nbPb}, ${nbReject/nbPb}, ${nbWithdraw/nbPb}, ${nbConfirm/nbPb}, ${nbInform/nbPb}\n")
           bw.flush()
