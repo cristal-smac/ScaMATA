c393ba004feb577bf6a8291e431515615dccf1ea
morge <Maxime.Morge@univ-lille1.fr>
2018-05-25 11:03:22 +0200

    Copare Cmax et Gift outcome with 5 tasks per worker

--- src/main/scala/org/scamata/experiments/ComputeCmax.scala
+++ src/main/scala/org/scamata/experiments/ComputeCmax.scala
@@ -21,30 +21,36 @@ object ComputeCmax {
       val file = s"experiments/data/min$rule.csv"
       val bw = new BufferedWriter(new FileWriter(file))
       bw.write(s"m,n," +
-        s"minexhaustiveSolver$rule,openexhaustiveSolver$rule,meanexhaustiveSolver$rule,closedexhaustiveSolver$rule,maxexhaustiveSolver$rule,\n")
+        s"minexhaustiveSolver$rule,openexhaustiveSolver$rule,meanexhaustiveSolver$rule,closedexhaustiveSolver$rule,maxexhaustiveSolver$rule," +
+      "mingiftSolver$rule,opengiftSolver$rule,meangiftSolver$rule,closedgiftSolver$rule,maxgiftSolver$rule,\n")
       for (m <- 2 to 100) {
         for (n <- 2*m to 2*m) {
           if (debug) println(s"TestCmax configuration with $m peers and $n tasks")
           val nbPb = 100 // should be x*4
-          var (exhaustiveSolverRule, exhaustiveSolverTime) =
-            (List[Double](), List[Double]())
+          var (exhaustiveSolverRule, exhaustiveSolverTime, giftSolverRule, giftSolverTime) =
+            (List[Double](), List[Double](), List[Double](), List[Double]())
           for (o <- 1 to nbPb) {
             val pb = MWTA.randomProblem(m, n)
             val exhaustiveSolver = new ExhaustiveSolver(pb, rule)
             val exhaustiveAlloc = exhaustiveSolver.run()
-            rule match {
-                case Cmax =>
-                  exhaustiveSolverRule ::= exhaustiveAlloc.makespan()
-                case Flowtime =>
-                  exhaustiveSolverRule ::= exhaustiveAlloc.flowtime()
-            }
+            val giftSolver = new GiftSolver(pb, rule)
+            val giftAlloc = giftSolver.run()
+            exhaustiveSolverRule ::= exhaustiveAlloc.makespan()
+            giftSolverRule ::= giftAlloc.makespan()
             exhaustiveSolverTime ::= exhaustiveSolver.solvingTime
+            giftSolverTime ::= giftSolver.solvingTime
+
           }
           exhaustiveSolverRule = exhaustiveSolverRule.sortWith(_ < _)
           exhaustiveSolverTime = exhaustiveSolverTime.sortWith(_ < _)
+          giftSolverRule = giftSolverRule.sortWith(_ < _)
+          giftSolverTime = giftSolverTime.sortWith(_ < _)
           bw.write(
-            s"$m,$n,${exhaustiveSolverRule.min},${exhaustiveSolverRule(nbPb/4)},${exhaustiveSolverRule(nbPb/2)},${exhaustiveSolverRule(nbPb*3/4)},${exhaustiveSolverRule.max}," +
-            s"${exhaustiveSolverTime.min},${exhaustiveSolverTime(nbPb/4)},${exhaustiveSolverTime(nbPb/2)},${exhaustiveSolverTime(nbPb*3/4)},${exhaustiveSolverTime.max},\n")
+            s"$m,$n,"+
+              "${exhaustiveSolverRule.min},${exhaustiveSolverRule(nbPb/4)},${exhaustiveSolverRule(nbPb/2)},${exhaustiveSolverRule(nbPb*3/4)},${exhaustiveSolverRule.max}," +
+              "${giftSolverRule.min},${giftSolverRule(nbPb/4)},${giftSolverRule(nbPb/2)},${giftSolverRule(nbPb*3/4)},${giftSolverRule.max}," +
+              "${exhaustiveSolverTime.min},${exhaustiveSolverTime(nbPb/4)},${exhaustiveSolverTime(nbPb/2)},${exhaustiveSolverTime(nbPb*3/4)},${exhaustiveSolverTime.max}," +
+              "${giftSolverTime.min},${giftSolverTime(nbPb/4)},${giftSolverTime(nbPb/2)},${giftSolverTime(nbPb*3/4)},${giftSolverTime.max}\n")
           bw.flush()
         }
       }
