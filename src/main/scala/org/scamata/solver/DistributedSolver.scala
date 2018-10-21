685a260ad46ea9fb65feed7efdbe1b9c830dc334
Maxime MORGE <maxime.morge@univ-lille.fr>
2018-10-21 10:54:12 +0200

    increase timeout

--- src/main/scala/org/scamata/solver/DistributedSolver.scala
+++ src/main/scala/org/scamata/solver/DistributedSolver.scala
@@ -20,7 +20,7 @@ import scala.language.postfixOps
   */
 class DistributedSolver(pb : MATA, rule : SocialRule, strategy : DealStrategy, system: ActorSystem) extends DealSolver(pb, rule, strategy) {
 
-  val TIMEOUTVALUE : FiniteDuration = 600 minutes // Default timeout of a run
+  val TIMEOUTVALUE : FiniteDuration = 6000 minutes // Default timeout of a run
   implicit val timeout : Timeout = Timeout(TIMEOUTVALUE)
   // Launch a new solverAgent
   DistributedSolver.id+=1
