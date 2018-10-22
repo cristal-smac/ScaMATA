2ff0b4236884902d164a39ef58e5b29650f120fe
Maxime MORGE <maxime.morge@univ-lille.fr>
2018-10-22 16:11:12 +0200

    Compute Cmax with TaskCorrelated

--- src/main/scala/org/scamata/experiments/ComputeCmax.scala
+++ src/main/scala/org/scamata/experiments/ComputeCmax.scala
@@ -23,7 +23,7 @@ object ComputeCmax {
         s"mingiftSolver$rule,opengiftSolver$rule,meangiftSolver$rule,closedgiftSolver$rule,maxgiftSolver$rule,"+
         s"minswapSolver$rule,openswapSolver$rule,meanswapSolver$rule,closedswapSolver$rule,maswapSolver$rule,"+
         s"minectSolver$rule,openectSolver$rule,meanectSolver$rule,closedectSolver$rule,maxectSolver$rule\n")
-      for (m <- 2 to 100) {
+      for (m <- 2 to 100 by 2) {
         for (n <- 5 to 5) {
           if (debug) println(s"Test configuration with $m peers and $n tasks")
           val nbPb = 20 // should be x*4
