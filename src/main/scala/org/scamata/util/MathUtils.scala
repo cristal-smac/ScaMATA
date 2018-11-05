a8c20d24abc9ddf99a89958a097afb6415e1ae81
Maxime MORGE <maxime.morge@univ-lille.fr>
2018-11-05 16:40:00 +0100

    Test + variance/mean time/rule

--- src/main/scala/org/scamata/util/MathUtils.scala
+++ src/main/scala/org/scamata/util/MathUtils.scala
@@ -99,7 +99,7 @@ object Stat{
     * @param values of the random variable
     */
   def variance(values: List[Double]) : Double = {
-    val mean = mean(values)
+    val mean : Double = mean(values)
     values.map(a => math.pow(a - mean, 2)).sum / values.length
   }
 
